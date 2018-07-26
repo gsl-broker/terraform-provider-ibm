9c6773032393b54f6e230e06b80fc7e710860573
sumit-tembe-ibm <36759179+sumit-tembe-ibm@users.noreply.github.com>
2018-07-26 17:00:24 +0530

    bump up version to v0.11

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Version is the current provider main version
-const Version = "0.10.9"
+const Version = "0.11"
 
 // GitCommit is the git commit that was compiled. This will be filled in by the compiler.
 var GitCommit string
