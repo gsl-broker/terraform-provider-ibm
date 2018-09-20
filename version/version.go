d01e9332ac0307af4837135ae0e6d6eca0a6412e
danish5606 <danish.nizami.khan@gmail.com>
2018-09-20 16:35:21 +0530

    Update version.go
    0.11.7

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Version is the current provider main version
-const Version = "0.11.6"
+const Version = "0.11.7"
 
 // GitCommit is the git commit that was compiled. This will be filled in by the compiler.
 var GitCommit string
